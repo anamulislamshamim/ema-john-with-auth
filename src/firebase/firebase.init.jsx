9caaa18ad66653dbe749d498e5a25bbee7f50d5a
anamulislamshamim <anamulislamshamim117@gmail.com>
2022-10-16 21:23:22 +0600

    the project environment has been created successfully

---
+++ src/firebase/firebase.init.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+// Import the functions you need from the SDKs you need
+import { faAppleAlt } from "@fortawesome/free-solid-svg-icons";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_xRKjFaf2HXhPCf6CxKOfkMsFmIVjI-M",
+  authDomain: "ema-john-with-auth-89e55.firebaseapp.com",
+  projectId: "ema-john-with-auth-89e55",
+  storageBucket: "ema-john-with-auth-89e55.appspot.com",
+  messagingSenderId: "424697757396",
+  appId: "1:424697757396:web:0fc5d713b209ba34f40541"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
